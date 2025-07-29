9c667908e1b48399ceda238ee629ddef7bed1ac3
jdai-explore <jdaiexplore@gmail.com>
2025-07-30 00:52:28 +0200

    minor issues fix

--- odxtools/gui/main.py
+++ odxtools/gui/main.py
@@ -1,12 +1,12 @@
 import sys
 import odxtools
-import odxtools.ecu
-import odxtools.service
-
+from odxtools.diaglayers.diaglayer import DiagLayer
+from odxtools.diagservice import DiagService as Service
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QAction, QStandardItemModel, QStandardItem
 from PySide6.QtWidgets import (
     QApplication,
+    QDockWidget,
     QFileDialog,
     QMainWindow,
     QTextEdit,
@@ -46,18 +46,30 @@ class MainWindow(QMainWindow):
         self.statusBar().showMessage("Ready")
 
     def create_widgets(self):
+        # Database Explorer is already a QDockWidget
         self.database_explorer = DatabaseExplorer()
         self.addDockWidget(Qt.LeftDockWidgetArea, self.database_explorer)
-        self.database_explorer.tree_view.selectionModel().selectionChanged.connect(self.item_selected)
 
+        # Create and add Service Editor dock widget
         self.service_editor = ServiceEditor()
-        self.addDockWidget(Qt.RightDockWidgetArea, self.service_editor)
+        service_dock = QDockWidget("Service Editor", self)
+        service_dock.setWidget(self.service_editor)
+        self.addDockWidget(Qt.RightDockWidgetArea, service_dock)
 
+        # Create and add DTC Viewer dock widget
         self.dtc_viewer = DTCViewer()
-        self.addDockWidget(Qt.BottomDockWidgetArea, self.dtc_viewer)
+        dtc_dock = QDockWidget("DTC Viewer", self)
+        dtc_dock.setWidget(self.dtc_viewer)
+        self.addDockWidget(Qt.BottomDockWidgetArea, dtc_dock)
 
+        # Create and add Communication Console dock widget
         self.communication_console = CommunicationConsole()
-        self.tabifyDockWidget(self.dtc_viewer, self.communication_console)
+        console_dock = QDockWidget("Communication Console", self)
+        console_dock.setWidget(self.communication_console)
+        self.addDockWidget(Qt.BottomDockWidgetArea, console_dock)
+        
+        # Tabify the bottom dock widgets
+        self.tabifyDockWidget(dtc_dock, console_dock)
 
     def open_file(self):
         file_name, _ = QFileDialog.getOpenFileName(
@@ -78,25 +90,27 @@ class MainWindow(QMainWindow):
         index = selected.indexes()[0]
         item_data = index.data(Qt.UserRole)
 
-        if isinstance(item_data, odxtools.ecu.Ecu):
+        if isinstance(item_data, DiagLayer):
             self.dtc_viewer.set_ecu(item_data)
-        elif isinstance(item_data, odxtools.service.Service):
+        elif isinstance(item_data, Service):
             self.service_editor.set_service(item_data)
 
     def populate_database_explorer(self, db):
         model = QStandardItemModel()
         self.database_explorer.tree_view.setModel(model)
+        self.database_explorer.tree_view.selectionModel().selectionChanged.connect(self.item_selected)
 
         for ecu in db.ecus:
             ecu_item = QStandardItem(ecu.short_name)
             ecu_item.setData(ecu, Qt.UserRole)
             model.appendRow(ecu_item)
 
-            diag_layer = ecu.diag_layer
-            for service in diag_layer.services:
-                service_item = QStandardItem(service.short_name)
-                service_item.setData(service, Qt.UserRole)
-                ecu_item.appendRow(service_item)
+            diag_layer = ecu.diag_layer_raw
+            if hasattr(diag_layer, 'services'):
+                for service in diag_layer.services:
+                    service_item = QStandardItem(service.short_name)
+                    service_item.setData(service, Qt.UserRole)
+                    ecu_item.appendRow(service_item)
 
 
 def main():
